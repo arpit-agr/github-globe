93909bd603b38c200fc7d2ef0c9423fd55d9b902
arpit-agr <arpit.agrawal2502@gmail.com>
2022-02-24 22:40:52 +0530

    add cobe web gl and products section to index.html

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,24 @@
+const esbuild = require("esbuild");
+
 module.exports = function (eleventyConfig) {
 
-
 	//PASSTHROUGH COPY
-	eleventyConfig.addPassthroughCopy("./src/css/");
-	eleventyConfig.addPassthroughCopy("./src/scripts/");
 	eleventyConfig.addPassthroughCopy("./src/img/");
 
 
 	//WATCH TARGET
 	eleventyConfig.addWatchTarget("./src/css/");
+	eleventyConfig.addWatchTarget('./src/scripts/')
+
+	//ELEVENTY AFTER EVENT
+	eleventyConfig.on('eleventy.after', async () => {
+    // Run me after the build ends
+		return esbuild.build({
+      entryPoints: ['src/scripts/globe.js'],
+      bundle: true,
+      outdir: 'public/scripts'
+    });
+  });
 
 
 	return {
---
+++ src/scripts/globe.js
@@ -0,0 +1,38 @@
+import createGlobe from "cobe";
+
+let phi = 3.5;
+let canvas = document.getElementById("cobe");
+
+const globe = createGlobe(canvas, {
+  devicePixelRatio: 2,
+  width: 600 * 2,
+  height: 600 * 2,
+  phi: 0,
+  theta: 0,
+  dark: 1,
+  diffuse: 1.2,
+  mapSamples: 16000,
+  mapBrightness: 6,
+  baseColor: [0.05490196, 0.11764706, 0.14509804],
+  markerColor: [1, 1, 1],
+  glowColor: [0.05490196, 0.11764706, 0.14509804],
+  markers: [
+    // latitude longitude
+    { location: [19.075983, 72.877655], size: 0.05 },
+    { location: [1.290270, 103.851959], size: 0.05 },
+    { location: [35.893955, 117.924896], size: 0.05 }
+  ],
+  onRender: (state) => {
+    // Called on every animation frame.
+    // `state` will be an empty object, return updated params.
+    state.phi = phi;
+    phi -= 0.005;
+  }
+});
+
+// `globe` will be a Phenomenon (https://github.com/vaneenige/phenomenon) instance.
+// To pause requestAnimationFrame:
+// `globe.toggle()`
+// To remove the instance:
+// `globe.destroy()`
+// ...
