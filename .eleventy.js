f82500f66af6262f2a3e05943e95bb2f7521e1f8
arpit-agr <arpit.agrawal2502@gmail.com>
2022-02-25 18:11:03 +0530

    Add and configure 11ty image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,33 @@
 const esbuild = require("esbuild");
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [640, 800],
+    formats: ["avif", "webp", "jpeg"],
+		sizes: ["(max-width: 480px) 100vw", "(max-width: 961px) 50vw", "33vw"],
+		outputDir: "./public/img/"
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 
 module.exports = function (eleventyConfig) {
 
 	//PASSTHROUGH COPY
 	eleventyConfig.addPassthroughCopy("./src/img/");
 
+	//SHORTCODE
+	eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
 	//WATCH TARGET
 	eleventyConfig.addWatchTarget("./src/css/");
@@ -22,6 +45,8 @@ module.exports = function (eleventyConfig) {
 
 
 	return {
+		htmlTemplateEngine: "njk",
+		markdownTemplateEngine: "njk",
 		dir: {
 			input: 'src',
 			output: 'public'
