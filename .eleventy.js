ecfc1813cb90ddebc70e8f30018ea0c9416dc751
arpit-agr <arpit.agrawal2502@gmail.com>
2022-04-02 17:23:57 +0530

    add post date eleventy filter

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,16 @@ const esbuild = require("esbuild");
 const imageShortcode = require("./src/_11ty/shortcodes/image");
 const htmlmin = require("html-minifier");
 const CleanCSS = require("clean-css");
+const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+	});
 
 	//PASSTHROUGH COPY
 	eleventyConfig.addPassthroughCopy("./src/img");
