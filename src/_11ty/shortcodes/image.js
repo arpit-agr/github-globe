89fed992b2fe423a021b46bcfc712a6031ce221c
arpit-agr <arpit.agrawal2502@gmail.com>
2022-05-17 12:29:59 +0530

    update image shortcode

--- src/_11ty/shortcodes/image.js
+++ src/_11ty/shortcodes/image.js
@@ -2,7 +2,13 @@ const Image = require("@11ty/eleventy-img");
 
 //Add default value for size attr https://github.com/AleksandrHovhannisyan/11ty-sass-images-seo/blob/master/11ty/shortcodes/image.js
 
-async function imageShortcode(src, alt, sizes = '(max-width: 480px) 100vw, (max-width: 961px) 50vw, 33vw', loading) {
+async function imageShortcode(
+  src, 
+  alt, 
+  sizes = '(max-width: 480px) 100vw, (max-width: 961px) 50vw, 33vw', 
+  loading, 
+  className = ''
+  ) {
   let metadata = await Image(src, {
     widths: [640, 800],
     formats: ["avif", "webp", "jpeg"],
@@ -10,6 +16,7 @@ async function imageShortcode(src, alt, sizes = '(max-width: 480px) 100vw, (max-
   });
 
   let imageAttributes = {
+    class: className,
     alt,
     sizes,
     loading,
